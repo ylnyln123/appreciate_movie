67a24200d21b06856c25e540b6b19cf42ff15bf2
Bunanas1222 <bunanas1997@gmail.com>
2021-03-24 22:55:46 +0800

    错误分支

--- pages/blog/blog.js
+++ pages/blog/blog.js
@@ -5,12 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+   modalShow:false,
+    blogList:[],
   },
   //发布
   onPublish(){
+    //判断用户是否授权
+    wx.getSetting({
+      success :(res)=> {
+        console.log(res)
+        if(res.authSetting['scope.userInfo']){
+          wx.getUserInfo({
+            success:(res) =>{
+              this.onLoginSuccess({
+                detail:res.userInfo
+              })
+            }
+          })
+      }else{
+        this.setData({
+          modalShow:true,
+        })
+      }
+  }
+})
+},
+onLoginSuccess(event){
+  console.log(event)
+  const detail=event.detail
+  wx.navigateTo({
+    url: `../blog-edit/blog-edit?nickName=${detail.nickName}&avatarUrl=${detail.avatarUrl}`,
+  })
+},
+onLoginFail(){
+  wx.showModal({
+    title: '授权用户才能发布',
+    content: '',
+  })
+},
 
-  },
   /**
    * 生命周期函数--监听页面加载
    */
